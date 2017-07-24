333b785288bf99f8e51c1a5ca5234c1b4ad69199
bysslord <wxwlegend@gmail.com>
2017-07-25 00:31:58 +0800

    add setting box

--- iobox.py
+++ iobox.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+from PyQt5 import Qt
+from PyQt5.QtGui import QCloseEvent
+from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QSystemTrayIcon, QApplication, QMenu
+from PyQt5.QtWidgets import QSystemTrayIcon, QApplication, QMenu, \
+    QDialog, QProgressBar, QLabel, QPushButton, QInputDialog
 
 
 __author__ = 'xiwei'
@@ -15,35 +19,70 @@ class SysTray(QSystemTrayIcon):
     sys tray
     """
 
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
+        self.parent = parent  # type: SettingBox
         self.menu = QMenu()
         self.icon = QIcon('logo.png')
-        self.settings = QSettings()
-        self.initUI()
+        self.init_ui()
 
-    def initMenu(self):
+    def init_menu(self):
         """
         init menu
         """
-        self.menu.addAction('alert', self.test)
-        self.menu.addAction('退出', app.quit)
+        self.menu.addAction('设置', self.setting)
+        self.menu.addSeparator()
+        self.menu.addAction('退出', self.exit)
         self.setIcon(self.icon)
         self.setContextMenu(self.menu)
 
-    def test(self):
-        self.alert('iobox', 'test')
+    def exit(self):
+        self.hide()
+        app.quit()
+
+    def setting(self):
+        """
+        open setting box
+        :return:
+        """
+        self.parent.show()
 
     def alert(self, title: str, content: str):
-        print(title, content)
         self.showMessage(title, content)
 
-    def initUI(self):
+    def init_ui(self):
         """
         init ui
         """
-        self.initMenu()
+        self.init_menu()
         self.show()
+
+
+class SettingBox(QDialog):
+    progressBarSpace: QProgressBar
+    labelSpace: QLabel
+    pushButtonAccount: QPushButton
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(flags=Qt.Qt.WindowStaysOnTopHint, *args, **kwargs)
+        self.tray = SysTray(self)
+        self.settings = QSettings()
+        self.init_ui()
+
+    def init_ui(self):
+        loadUi('resource/setting.ui', self)
+
+        self.progressBarSpace.setVisible(False)
+        self.labelSpace.setVisible(False)
+        self.pushButtonAccount.clicked.connect(self.login)
+        self.tray.show()
+
+    def login(self):
+        res = QInputDialog(self)
+
+    def closeEvent(self, event: QCloseEvent):
+        event.ignore()
+        self.hide()
 
 
 if __name__ == '__main__':
@@ -52,6 +91,6 @@ if __name__ == '__main__':
     app.setOrganizationName('IoNull')
     app.setOrganizationDomain('ionull.com')
     app.setApplicationName('IoBox')
-    tray = SysTray()
+    tray = SettingBox(None)
     sys.exit(app.exec_())
 
