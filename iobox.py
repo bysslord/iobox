c6388994b4fa02c455bca8236c7196d70e6c0916
bysslord <wxwlegend@gmail.com>
2017-07-26 00:29:43 +0800

    add login box, add stylesheet

--- iobox.py
+++ iobox.py
@@ -7,7 +7,7 @@ from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSettings
 from PyQt5.QtWidgets import QSystemTrayIcon, QApplication, QMenu, \
-    QDialog, QProgressBar, QLabel, QPushButton, QInputDialog
+    QDialog, QProgressBar, QLabel, QPushButton, QLineEdit
 
 
 __author__ = 'xiwei'
@@ -58,6 +58,31 @@ class SysTray(QSystemTrayIcon):
         self.show()
 
 
+class LoginBox(QDialog):
+    pushButtonLogin: QPushButton
+    pushButtonCancel: QPushButton
+    lineEditUsername: QLineEdit
+    lineEditPassword: QLineEdit
+
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent=parent, flags=Qt.Qt.WindowStaysOnTopHint, *args, **kwargs)
+        loadUi('resource/login.ui', self)
+        self.parent = parent  # type: SettingBox
+
+        self.pushButtonCancel.clicked.connect(self.close)
+        self.pushButtonLogin.clicked.connect(self.login)
+        self.lineEditUsername.setText(self.parent.settings.value('username', ''))
+        self.lineEditPassword.setText(self.parent.settings.value('password', ''))
+
+    def login(self):
+        username = self.lineEditUsername.text()
+        password = self.lineEditPassword.text()
+        self.parent.settings.setValue('username', username)
+        self.parent.settings.setValue('password', password)
+        self.parent.check_account()
+        self.close()
+
+
 class SettingBox(QDialog):
     progressBarSpace: QProgressBar
     labelSpace: QLabel
@@ -68,17 +93,35 @@ class SettingBox(QDialog):
         self.tray = SysTray(self)
         self.settings = QSettings()
         self.init_ui()
+        self.check_account()
 
     def init_ui(self):
         loadUi('resource/setting.ui', self)
 
-        self.progressBarSpace.setVisible(False)
-        self.labelSpace.setVisible(False)
         self.pushButtonAccount.clicked.connect(self.login)
         self.tray.show()
 
+    def check_account(self):
+        if self.username:
+            self.pushButtonAccount.setText(f"切换账户:{self.username}")
+            self.progressBarSpace.setVisible(True)
+            self.progressBarSpace.setDisabled(False)
+            self.progressBarSpace.setMaximum(50)
+            self.progressBarSpace.setValue(13)
+            self.labelSpace.setVisible(True)
+        else:
+            self.pushButtonAccount.setText("登录")
+            self.progressBarSpace.setVisible(False)
+            self.labelSpace.setVisible(False)
+            self.show()
+
+    @property
+    def username(self):
+        return self.settings.value('username', False)
+
     def login(self):
-        res = QInputDialog(self)
+        res = LoginBox(self)
+        res.show()
 
     def closeEvent(self, event: QCloseEvent):
         event.ignore()
@@ -91,6 +134,8 @@ if __name__ == '__main__':
     app.setOrganizationName('IoNull')
     app.setOrganizationDomain('ionull.com')
     app.setApplicationName('IoBox')
+    with open('resource/style.qss', 'r') as style:
+        app.setStyleSheet(style.read())
     tray = SettingBox(None)
     sys.exit(app.exec_())
 
